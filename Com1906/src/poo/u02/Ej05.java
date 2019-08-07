0c46e12c904408c3a3acb588512e6d84af53849f
alumno <alumno@DESKTOP-CNRL87V>
2019-08-07 14:07:18 -0300

    @ramirocodoacodo Mi20190807

--- Com1906/src/poo/u02/Ej05.java
+++ Com1906/src/poo/u02/Ej05.java
@@ -3,7 +3,7 @@ package poo.u02;
 public class Ej05 {
 
 	public static void main(String[] args) {
-		DiscoMusical disco1 = new DiscoMusical();
+		DiscoMusical disco1  = new DiscoMusical();
 		System.out.println(disco1.toString());
 		DiscoMusical disco2 = new DiscoMusical("Nevermind", "Nirvana", 1991);
 		System.out.println(disco2.toString());
