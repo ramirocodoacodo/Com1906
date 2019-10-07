856a741517e6ad2015ecdbd5ffd3aaa206060c24
alumno <alumno@DESKTOP-CNRL87V>
2019-10-07 16:58:16 -0300

    @ramirocodoacodo Lu20191007

--- Com1906/src/poo/u07/EjemploExcepciones.java
+++ Com1906/src/poo/u07/EjemploExcepciones.java
@@ -3,6 +3,7 @@ package poo.u07;
 import java.util.Scanner;
 
 public class EjemploExcepciones {
+	
 	public static int validarEdad(int edad) throws IntervaloEdadException {
 		if (edad < 0 || edad > 120) {
 			throw new IntervaloEdadException("Edad no válida");
@@ -12,6 +13,7 @@ public class EjemploExcepciones {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
+		System.out.println("Ingrese su edad: ");
 		String edadStr = teclado.nextLine();
 		int edad;
 
@@ -20,9 +22,10 @@ public class EjemploExcepciones {
 			edad = validarEdad(edad);
 			System.out.println("La edad ingresada es: " + edad);
 		} catch (NumberFormatException e1) {
-			System.out.println("Error en el número ingresado");
+			System.out.println("Error en el formato del número ingresado");
 		} catch (IntervaloEdadException e2) {
-			System.out.println("Intervalo de edad fuera de rango");
+			//System.out.println("Intervalo de edad fuera de rango");
+			System.out.println(e2.getMessage());
 		} catch (Exception e3) {
 			System.out.println("Error no esperado!");
 		}
